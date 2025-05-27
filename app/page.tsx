f3441b706e95cce1f70feddc90b6f06f1b33b8f2
Nishul Dhakar <nishuldhakar.se@gmail.com>
2025-05-27 21:49:40 +0530

    added plausible

--- app/page.tsx
+++ app/page.tsx
@@ -2,13 +2,19 @@
 
 import DeveloperHero from "./Components/mainCard";
 import NameEffect from "./Components/nameEffect";
+import Script from "next/script";
 
 
 export default function Home() {
 
   return (
     <>
-    <script defer data-domain="nishul.dev" src="https://plausible.io/js/script.js"></script>
+     <Script
+        strategy="beforeInteractive"
+        defer
+        data-domain="nishul.dev"
+        src="https://plausible.io/js/script.js"
+      />
 
     <div className="md:flex justify-between items-center w-full min-h-screen px-8 lg:px-28 py-8 relative overflow-hidden">
             <div>
